110fd892ed3ffbebdfa317fcccfc9076ff5e4963
DINSTORE99 <dinstoree41@gmail.com>
2026-08-15 11:55:48 +0700

    Create balance.js

---
+++ api/balance.js
@@ -0,0 +1,17 @@
+import { nokos } from "./_nokos.js";
+
+export default async function handler(req, res) {
+  try {
+    const result = await nokos("getBalance");
+
+    res.status(200).json({
+      success: true,
+      data: result.data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
